6b02b8dcac0a32e88d4856e89909de40a38055ba
Siddhu Mohan <61168189+Ranger-26@users.noreply.github.com>
2024-09-26 15:48:10 -0500

    yes

--- rfid_read.py
+++ rfid_read.py
@@ -5,4 +5,4 @@ while True:
     tag = device.read(16)  # Tries to read the ISO tag format made of 14 digits
     if len(tag) != 0:  # If something has been read, print it then break out of the loop
         print(tag)
-        break
+        
